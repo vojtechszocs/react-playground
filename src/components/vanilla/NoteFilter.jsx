7967b4386fac9cfc51080823b28c7a28ffb4a202
Vojtech Szocs <vojtech.szocs@gmail.com>
2017-05-06 20:12:54 +0200

    Use ESC to reset filter
    Signed-off-by: Vojtech Szocs <vojtech.szocs@gmail.com>

--- src/components/vanilla/NoteFilter.jsx
+++ src/components/vanilla/NoteFilter.jsx
@@ -3,17 +3,27 @@ import PropTypes from 'prop-types'
 
 import style from './NoteFilter.css'
 
-const NoteFilter = ({ onChange }) => (
-  <div className={style.root}>
+const NoteFilter = ({ onChange }) => {
+  function resetFilter (input) {
+    if (input.value) {
+      input.value = ''
+      onChange(input.value)
+    }
+  }
 
-    <span>Find notes:</span>
+  return (
+    <div className={style.root}>
 
-    <input type='text' className={style.input}
-           placeholder='Filter by text or tags'
-           onChange={event => { onChange(event.target.value) }} />
+      <span>Find notes (ESC to reset):</span>
 
-  </div>
-)
+      <input type='text' className={style.input}
+             placeholder='Filter by text or tags'
+             onChange={event => { onChange(event.target.value) }}
+             onKeyUp={event => { event.keyCode === 27 && resetFilter(event.target) }}/>
+
+    </div>
+  )
+}
 
 NoteFilter.propTypes = {
   onChange: PropTypes.func.isRequired
